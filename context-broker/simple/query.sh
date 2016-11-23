a2d9ab86c799443a613bc6327634a272283f4417
Tomas Aliaga <tomas.aliaga@gmail.com>
2016-11-23 12:36:42 +0100

    Move simple scripts to a folder

--- context-broker/simple/query.sh
+++ context-broker/simple/query.sh
@@ -1,1 +1,2 @@
 curl localhost:1026/v2/entities -s -S --header 'Accept: application/json' | python -mjson.tool
+curl localhost:1026/v2/entities -s -S --header 'Accept: application/json' | python -mjson.tool
