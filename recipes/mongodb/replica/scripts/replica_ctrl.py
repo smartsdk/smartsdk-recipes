d7c0a0bc0058a56ab5c0987c44e6a3f9404f1ac7
Tomas Aliaga <tomas.aliaga@gmail.com>
2017-02-09 16:03:22 +0100

    Check env variables are properly set

--- recipes/mongodb/replica/scripts/replica_ctrl.py
+++ recipes/mongodb/replica/scripts/replica_ctrl.py
@@ -24,92 +24,105 @@ recognize the one already running.
 """
 import docker
 import logging
+import os
 import pymongo as pm
 import time
 
-# TODO: Link these to env variables in docker-compose.yml
-STACK_NAME = 'mongo-replica'
-OVERLAY_NETWORK_NAME = '{}_replica'.format(STACK_NAME)
-SERVICE_NAME_MONGO = '{}_mongo'.format(STACK_NAME)
-REPLICASET_NAME = 'rs'
-MONGO_PORT = 27017
+
+def get_required_env_variables():
+    REQUIRED_VARS = [
+        'OVERLAY_NETWORK_NAME',
+        'MONGO_SERVICE_NAME',
+        'REPLICASET_NAME',
+        'MONGO_PORT'
+    ]
+    envs = {}
+    for rv in REQUIRED_VARS:
+        envs[rv.lower()] = os.environ[rv]
+
+    if not all(envs.values()):
+        raise RuntimeError("Missing required ENV variables. {}".format(envs))
+
+    envs['mongo_port'] = int(envs['mongo_port'])
+    return envs
 
 
-def get_tasks_ips(tasks):
+def get_mongo_service(dc, mongo_service_name):
+    for s in dc.services.list():
+        if s.name == mongo_service_name:
+            return s
+    msg = "Error: Could not find mongo service. Did you correctly deploy the stack with both services?."
+    logger.error(msg, exc_info=True)
+    return
+
+
+def get_tasks_ips(tasks, overlay_network_name):
     tasks_ips = []
     for t in tasks:
         for n in t['NetworksAttachments']:
-            if n['Network']['Spec']['Name'] == OVERLAY_NETWORK_NAME:
-                # clean ip
-                ip = n['Addresses'][0].split('/')[0]
+            if n['Network']['Spec']['Name'] == overlay_network_name:
+                ip = n['Addresses'][0].split('/')[0]  # clean prefix from ip
                 tasks_ips.append(ip)
     return tasks_ips
 
 
-def create_mongo_config(tasks_ips):
+def create_mongo_config(tasks_ips, replicaset_name, mongo_port):
     members = []
     for i, ip in enumerate(tasks_ips):
         members.append({
           '_id': i,
-          'host': "{}:{}".format(ip, MONGO_PORT)
+          'host': "{}:{}".format(ip, mongo_port)
         })
-    config = {'_id': REPLICASET_NAME, 'members': members}
+    config = {
+        '_id': replicaset_name,
+        'members': members,
+        'version': 1
+    }
     return config
 
 
-def configure_replica(dc):
+def configure_replica(mongo_service, overlay_network_name, replicaset_name, mongo_port):
     logger = logging.getLogger(__name__)
 
-    # TODO: add healthcheck in docker-compose and remove this sleep
+    # TODO: add healthchecks with retries in docker-compose, here or both as required
+    # so as to remove this arbitrary sleep
     logger.info('Waiting some time before starting')
-    time.sleep(42)
+    time.sleep(142)
 
     # Get mongo tasks
-    services = {s.name:s for s in dc.services.list()}
-    if SERVICE_NAME_MONGO not in services:
-        msg = "Error: Could not find mongo service. Did you correctly deploy the stack with both services?."
-        logger.error(msg, exc_info=True)
-        return
-
-    mongo_service = services[SERVICE_NAME_MONGO]
     mongo_tasks = mongo_service.tasks()
     if len(mongo_tasks) == 0:
         msg = "Error: No task found for Mongo service. Missing a 'wait' time or healthcheck maybe?."
         logger.error(msg, exc_info=True)
         return
 
-    # Get overlay network IPS of running mongo daemons.
-    mongo_tasks_ips = get_tasks_ips(mongo_tasks)
-
     # Prepare mongo config
-    config = create_mongo_config(mongo_tasks_ips)
-    config['version'] = 1
+    mongo_tasks_ips = get_tasks_ips(mongo_tasks, overlay_network_name)
+    config = create_mongo_config(mongo_tasks_ips, replicaset_name, mongo_port)
     logger.info("Initial config: {}".format(config))
 
     # Choose a primary and configure replicaset
-    primary_ip = str(mongo_tasks_ips[0])
-    primary = pm.MongoClient(primary_ip, MONGO_PORT)
+    primary_ip = mongo_tasks_ips[0]
+    primary = pm.MongoClient(primary_ip, mongo_port)
     res = primary.admin.command("replSetInitiate", config)
     logger.info("replSetInitiate: {}".format(res))
 
     # Respond to changes
     current_ips = set(mongo_tasks_ips)
-
     while True:
         # TODO: Reacting to events is better than sleeping and polling
         time.sleep(42)
 
-        new_ips = set(get_tasks_ips(mongo_service.tasks()))
+        new_ips = set(get_tasks_ips(mongo_service.tasks(), overlay_network_name))
         if not new_ips.difference(current_ips):
-            pass
+            continue
         else:
+            # Actually not too different from what mongo does:
+            # https://github.com/mongodb/mongo/blob/master/src/mongo/shell/utils.js
             to_remove = set(current_ips) - set(new_ips)
             to_add = set(new_ips) - set(current_ips)
             assert to_remove or to_add
 
-            # Reconfigure mongo replicaset
-            # Actually not too different from what mongo does:
-            # https://github.com/mongodb/mongo/blob/master/src/mongo/shell/utils.js
             if to_remove:
                 # Note: As of writing, when a node goes down with a task running
                 # a global service, Swarm is not tearing down that task and hence
@@ -124,7 +137,7 @@ def configure_replica(dc):
                 for i, ip in enumerate(to_add):
                     config['members'].append({
                       '_id': offset + i,
-                      'host': "{}:{}".format(ip, MONGO_PORT)
+                      'host': "{}:{}".format(ip, mongo_port)
                     })
 
             if primary_ip in to_remove:
@@ -134,6 +147,7 @@ def configure_replica(dc):
                 # primary = pm.MongoClient(primary_ip, MONGO_PORT)
                 raise NotImplementedError("TODO: Primary failure recovery")
 
+            # Apply new config
             config['version'] += 1
             logger.info("New config: {}".format(config))
             res = primary.admin.command("replSetReconfig", config)
@@ -144,5 +158,8 @@ def configure_replica(dc):
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
+
+    envs = get_required_env_variables()
     dc = docker.from_env()
-    configure_replica(dc)
+    mongo_service = get_mongo_service(dc, envs.pop('mongo_service_name'))
+    configure_replica(mongo_service, **envs)
