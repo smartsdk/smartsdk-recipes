1579a1d3972aa1b2837504d8aa6d7fe467b00726
Tomas Aliaga <tomas.aliaga@gmail.com>
2017-10-11 10:58:58 -0500

    Minor fixes

--- recipes/data-management/context-broker/ha/deploy_back.sh
+++ recipes/data-management/context-broker/ha/deploy_back.sh
@@ -2,6 +2,6 @@
 set -o allexport
 . ./backend.env
 set +o allexport
-docker network create --opt com.docker.network.driver.mtu=${DOCKER_MTU:-1500} -d overlay backend
-curl -O https://raw.githubusercontent.com/smartsdk/mongo-rs-controller-swarm/master/docker-compose.yml
-docker stack deploy -c docker-compose.yml ${STACK_NAME}
+curl https://raw.githubusercontent.com/smartsdk/mongo-rs-controller-swarm/master/docker-compose.yml -o docker-compose-mongo.yml
+curl -O https://raw.githubusercontent.com/smartsdk/mongo-rs-controller-swarm/master/mongo-healthcheck
+docker stack deploy -c docker-compose-mongo.yml ${STACK_NAME}
