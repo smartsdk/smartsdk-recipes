df70046371ef146fd77a1634f47c8da8c16e3bc3
Tomas <taliaga@users.noreply.github.com>
2017-11-13 07:51:40 +0100

    Small fixes (#9)

--- recipes/utils/mongo-replicaset/deploy.sh
+++ recipes/utils/mongo-replicaset/deploy.sh
@@ -4,4 +4,5 @@ set -o allexport
 set +o allexport
 docker network create --opt com.docker.network.driver.mtu=${DOCKER_MTU:-1500} -d overlay backend
 curl -O https://raw.githubusercontent.com/smartsdk/mongo-rs-controller-swarm/master/docker-compose.yml
+curl -O https://raw.githubusercontent.com/smartsdk/mongo-rs-controller-swarm/master/mongo-healthcheck
 docker stack deploy -c docker-compose.yml ${STACK_NAME}
