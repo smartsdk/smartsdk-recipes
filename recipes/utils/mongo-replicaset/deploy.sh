1f884340dc05796ace10aa326a913c3454dd752e
Tomas Aliaga <tomas.aliaga@gmail.com>
2017-11-17 12:07:42 +0100

    Make 1400 the default MTU to play safe in Fiware Lab

--- recipes/utils/mongo-replicaset/deploy.sh
+++ recipes/utils/mongo-replicaset/deploy.sh
@@ -2,7 +2,7 @@
 set -o allexport
 . ./backend.env
 set +o allexport
-docker network create --opt com.docker.network.driver.mtu=${DOCKER_MTU:-1500} -d overlay backend
+docker network create --opt com.docker.network.driver.mtu=${DOCKER_MTU:-1400} -d overlay backend
 curl -O https://raw.githubusercontent.com/smartsdk/mongo-rs-controller-swarm/master/docker-compose.yml
 curl -O https://raw.githubusercontent.com/smartsdk/mongo-rs-controller-swarm/master/mongo-healthcheck
 docker stack deploy -c docker-compose.yml ${STACK_NAME}
